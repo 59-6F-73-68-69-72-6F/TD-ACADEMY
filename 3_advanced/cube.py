f6b225a94e1257b826a23c93273bfb47d7692a10
Yoshiro <rudyleti@gmail.com>
2025-07-03 14:27:30 -0400

    DONE 3_advanced

--- 3_advanced/cube.py
+++ 3_advanced/cube.py
@@ -1,8 +1,8 @@
 # ADVANCED ***************************************************************************
 # content = assignment
 #
-# date    = 2022-01-07
-# email   = contact@alexanderrichtertd.com
+# date    = 2025-07-02
+# email   = rudyleti@gmail.com
 #************************************************************************************
 
 """
@@ -27,3 +27,80 @@ CUBE CLASS
    Update the Cube class to not repeat the content of Object.
 
 """
+
+import pprint as pp
+
+
+class Object:
+   def __init__(self, name):
+      self.name = name
+      self.tx = 0.0
+      self.ty = 0.0
+      self.tz = 0.0
+      self.rx = 0.0
+      self.ry = 0.0
+      self.rz = 0.0
+      self.sx = 1.0
+      self.sy = 1.0
+      self.sz = 1.0
+      self.r = 255
+      self.g = 255
+      self.b = 255
+      
+   def translate(self, x: float, y: float, z: float) -> None:
+      self.tx = x
+      self.ty = y
+      self.tz = z
+
+   def rotate(self, x: float, y: float, z: float) -> None:
+      self.rx = x
+      self.ry = y
+      self.rz = z
+
+   def scale(self, x: float, y: float, z: float) -> None:
+      self.sx = x
+      self.sy = y
+      self.sz = z
+   
+   def update_transform(self, ttype, *value) -> None:
+      transform_methods = {
+         "translate": self.translate,
+         "rotate": self.rotate,
+         "scale": self.scale
+      } 
+      if ttype in transform_methods:
+         transform_methods[ttype](*value)
+   
+class Cube(Object):
+   
+   """
+   By inheriting from a parent 'Object'class,the Cube reuses common transformation logic (translate, rotate,scale) without duplicating code.
+   This makes it easy to create other types of objects (like a Sphere or Pyramid) that share the same basic transform properties.
+   """
+   
+   def __init__(self, name):
+      super().__init__(name)
+      self.name = name
+      
+   def color(self, r: int, g: int, b: int) -> None:
+      self.r = r
+      self.g = g
+      self.b = b
+      
+   def print_status(self):
+      status = {
+         "name": self.name,
+         "translate": (self.tx, self.ty, self.tz),
+         "rotate": (self.rx, self.ry, self.rz),
+         "scale": (self.sx, self.sy, self.sz),
+         "color": (self.r, self.g, self.b)
+         }
+      pp.pprint(status,sort_dicts=False)
+ 
+ # Example usage     
+cube1 = Cube("Cube1")
+cube1.color(255, 0, 0)
+cube1.update_transform("translate", 1.0, 2.0, 3.0)
+cube1.update_transform("rotate", 45.0, 90.0, 180.0)
+cube1.update_transform("scale", 2.0, 2.0, 2.0)
+cube1.print_status()
